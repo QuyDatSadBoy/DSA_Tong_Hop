13a42ec456f910fc5ca2661594790842cc8243ce
Quy Dat yl09 <dattqyl09@gmail.com>
2023-03-29 15:12:14 +0700

    nghịch đảo modul

--- Nghich-dao-modul.cpp
+++ Nghich-dao-modul.cpp
@@ -10,7 +10,26 @@ const ll mod = (ll)1e9 + 7;
     cout.tie(0);
 using namespace std;
 
+ll x, y, g;
+void extend_gcd(ll a, ll b)
+{
+    if (b == 0)
+    {
+        g = a;
+        x = 1;
+        y = 0;
+    }
+    else
+    {
+        extend_gcd(b, a % b);
+        ll tam = x;
+        x = y;
+        y = tam - a / b * y;
+    }
+}
 int main()
 {
     faster();
+    extend_gcd(24, 17);
+    cout << g << " " << x << " " << y << endl;
 }
