237afefad99839f37c2da13288430f81233983f3
Quy Dat yl09 <dattqyl09@gmail.com>
2023-03-30 00:49:08 +0700

    Dùng để đếm mảng con liên tiếp hay

---
+++ Sort_mang_con_co_tong_bang_x_2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+const ll mod = (ll)1e9 + 7;
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define ms(a) memset(a, 0, sizeof(a))
+#define faster()                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+// Đếm số mảng con có tổng bằng x các phần tử liên tiếp
+
+int main()
+{
+    faster();
+    ll n, x;
+    cin >> n >> x;
+    ll a[n];
+    for (auto &x : a)
+        cin >> x;
+    map<ll, ll> mp;
+    mp[0] = 1;
+    ll ans = 0, sum = 0; // lưu kết quả
+    for (ll i = 0; i < n; i++)
+    {
+        sum += a[i];
+        if (mp[sum - x])
+            ans += mp[sum - x];
+        mp[sum]++;
+    }
+    cout << ans << endl;
+}
