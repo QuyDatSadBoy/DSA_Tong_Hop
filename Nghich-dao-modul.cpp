b61c2d8c36c2db7b3737c16717592ac5fc0716eb
Quy Dat yl09 <dattqyl09@gmail.com>
2023-03-29 15:28:37 +0700

    Nghịch đảo modul update

--- Nghich-dao-modul.cpp
+++ Nghich-dao-modul.cpp
@@ -27,9 +27,34 @@ void extend_gcd(ll a, ll b)
         y = tam - a / b * y;
     }
 }
+
+ll pow_mod(ll a, ll b, ll m)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    ll tam = pow_mod(a, b / 2, m);
+    if (b & 1)
+        return (tam % m) * (tam % m) % m * (a % m) % m;
+    else
+        return (tam % m) * (tam % m) % m;
+}
 int main()
 {
     faster();
     extend_gcd(24, 17);
     cout << g << " " << x << " " << y << endl;
+    if (g != 1)
+    {
+        cout << "khong ton tai nghich dao modul" << endl;
+    }
+    else
+    {
+        cout << "nghich dao modul la :" << x << endl;
+    }
+
+    // cách 2 là : nếu p là số nguyên tố và a không chia hết cho p thì nghịch đảo modul của a và b là a^(p-2)%p
+    // vd a=2,p=11;
+    cout << pow_mod(2, 11 - 2, 11) << endl;
 }
