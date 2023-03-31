7e7187d7547585e00ddb4daf3c4bae68c2f9a4bf
Quy Dat yl09 <dattqyl09@gmail.com>
2023-03-31 17:32:09 +0700

    test2

---
+++ test/test1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+const ll mod = (ll)1e9 + 7;
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+#define ms(a) memset(a, 0, sizeof(a))
+#define faster()                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+
+int main()
+{
+    faster();
+}
